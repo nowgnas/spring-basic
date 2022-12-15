4712fc5190a65af3426f940016d2c3e3c63d8246
nowgnas <marshmello.sw@gmail.com>
2022-12-16 05:13:06 +0900

    컨테이너에 등록된 모든 빈 조회

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,10 +9,14 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
     // 관심사 분리를 위한 AppConfig
 
+    @Bean
     public MemberService memberService() {
         // 어떤 구현체가 들어갈지 정해준다
         // 생성자를 통해 주입 된다
@@ -20,15 +24,18 @@ public class AppConfig {
         return new MemberServiceImpl(memberRepository()); // 역할을 정의
     }
 
-    private MemberRepository memberRepository() { // 구현체를 정의
+    @Bean
+    public MemberRepository memberRepository() { // 구현체를 정의
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() { // 역할
         return new OrderServiceImpl(discountPolicy(), memberRepository());
     }
 
-    private DiscountPolicy discountPolicy() { // 구현체
+    @Bean
+    public DiscountPolicy discountPolicy() { // 구현체
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy(); // 새로운 정률 할인 정책으로 수정하기
     }
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -3,15 +3,24 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
 
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
+//        AppConfig appConfig = new AppConfig();
+//
+//        MemberService memberService = appConfig.memberService(); // 인터페이스를 준다
+////        MemberService memberService = new MemberServiceImpl();
 
-        MemberService memberService = appConfig.memberService(); // 인터페이스를 준다
-//        MemberService memberService = new MemberServiceImpl();
+        /*
+         * AppConfig에 있는 Bean을 Spring 컨테이너에 등록해준다
+         */
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
 
         Member member = new Member(1L, "membera", Grade.VIP);
         memberService.join(member);
