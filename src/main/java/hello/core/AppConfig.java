430a8bb5f96ae75c7cc4258ef70a8de823499759
nowgnas <marshmello.sw@gmail.com>
2022-12-16 03:29:13 +0900

    AppConfig refactoring

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,6 +1,8 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -14,11 +16,19 @@ public class AppConfig {
         // 어떤 구현체가 들어갈지 정해준다
         // 생성자를 통해 주입 된다
         // MemoryMemberRepository 생성자를 주입해 준다고 표현한다 injection
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository()); // 역할을 정의
     }
 
-    public OrderService orderService() {
-        return new OrderServiceImpl(new FixDiscountPolicy(), new MemoryMemberRepository());
+    private MemberRepository memberRepository() { // 구현체를 정의
+        return new MemoryMemberRepository();
+    }
+
+    public OrderService orderService() { // 역할
+        return new OrderServiceImpl(discountPolicy(), memberRepository());
+    }
+
+    private DiscountPolicy discountPolicy() { // 구현체
+        return new FixDiscountPolicy();
     }
 
 }
