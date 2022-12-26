891576caaefd3fa1eab99d1a8fc4cdec26a7973c
nowgnas <marshmello.sw@gmail.com>
2022-12-26 23:48:10 +0900

    인터페이스 InitializingBean, DisposableBean

---
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -0,0 +1,25 @@
+package hello.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+    @Test
+    void lifeCycleTest() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient bean = ac.getBean(NetworkClient.class);
+        ac.close(); // close : 닫는 메서드 AnnotationConfigApplicationContext나 ConfigurableApplicationContext를 사용해야 한다
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+        @Bean
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http");
+            return networkClient;
+        }
+    }
+}
